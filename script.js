a3bbcbb40f057f985cc32e640c9376edbf209ca8
Bogdan <bogdanyushkov8@gmail.com>
2024-01-31 12:04:54 +0200

    add changeable rounds

--- script.js
+++ script.js
@@ -14,30 +14,51 @@ function getPlayerChoice() {
 }
 
 function playRound(ComputerChoice, PlayerChoice) {
+    let Draw = 0;
+    let Won = 0;
+    let Lose = 0;
     let lowerComputerChoise = ComputerChoice.toLocaleLowerCase();
     if (PlayerChoice == lowerComputerChoise) {
-        return alert(`It's a draw, computer choise was ${ComputerChoice}`)
+        alert(`It's a draw, computer choise was ${ComputerChoice}`)
+        ++Draw;
+        return console.log(`Draw: ${Draw}`)
     } else if (((PlayerChoice == "rock")&& (ComputerChoice =="Scissors"))) {
-        return alert("You won. Rock beats Scissors")
+        alert(`You won. Rock beats Scissors`)
+        ++Won;
+        return console.log(`Won: ${Won}`)
     } else if ((PlayerChoice == "paper") && (ComputerChoice =="Rock")) {
-        return alert("You won. Paper beats Rock")
+        alert(`You won. Paper beats Rock`)
+        ++Won;
+        return console.log(`Won: ${Won}`)
     } else if ((PlayerChoice =="scissors")&&(ComputerChoice=="Paper")){
-        return alert("You won. Scissors beats Paper")
+        alert("You won. Scissors beats Paper")
+        ++Won;
+        return console.log(`Won: ${Won}`)
     } else if (PlayerChoice == "Invalid Data, please enter your choice again"){
-        alert("try again, enter valid data");
+        alert("try again, you have entered invalid data");
         playRound(getComputerChoice(), getPlayerChoice());
     } else if ((PlayerChoice =="rock")&&(ComputerChoice=="Paper")) {
-        return alert("You lose. Paper beats Rock")
+        alert(`You lose. Paper beats Rock`)
+        ++Lose;
+        return console.log(`Lose: ${Lose}`)
     } else if ((PlayerChoice =="paper")&&(ComputerChoice=="Scissors")) {
-        return alert("You lose. Scissors beats Paper")
+        alert("You lose. Scissors beats Paper")
+        ++Lose;
+        return console.log(`Lose: ${Lose}`)
     } else if ((PlayerChoice =="scissors")&&(ComputerChoice=="Rock")) {
-        return alert("You lose. Rock beats Scissors")
-    } else {
-        alert(`You lose, computer choise was ${ComputerChoice}`)
+        alert("You lose. Rock beats Scissors")
+        ++Lose;
+        return console.log(`Lose: ${Lose}`)
+    } 
+}
+
+
+function playRounds(n) {
+    for (let i = 0; i < n; i++) {
+        playRound(getComputerChoice(), getPlayerChoice());
     }
 }
 
-playRound(getComputerChoice(), getPlayerChoice());
-
+playRounds(5);
 
 
